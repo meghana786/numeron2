b10d95f2e13cd3da6fc2c364aa647ddadb2269f8
meghana786 <160600002+meghana786@users.noreply.github.com>
2024-03-25 15:10:04 +0530

    score.js

--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 8: Making scoreboard functional
+var scoreBoard = document.getElementById("score-board");
+var score = localStorage.getItem('score');
+scoreBoard.textContent = score;
+
+
+play_again_button=document.getElementById("play-again-button");
+play_again_button.addEventListener("click", function()
+{
+  window.location.href = "game.html";
+});
